bd55c3efc50bbc935b2d68560f9907c9eef4f17d
Gangareddy Nachu <gnachu@horizon.csueastbay.edu>
2023-07-11 08:48:57 -0700

    Create RemoveDecimals.js

---
+++ RemoveDecimals.js
@@ -0,0 +1,17 @@
+
+let decimal = 4.51;
+//Integer a = 4;
+console.log(Math.trunc(decimal)); //4
+//console.log(a)
+
+//DoubleNot Operator
+console.log(~~4.51222); //4
+
+//Bitwise Operator
+console.log(5.32|0); //5
+
+//rightshift
+console.log(6.888>>0);
+
+//leftshift
+console.log(4.22222<<0);
