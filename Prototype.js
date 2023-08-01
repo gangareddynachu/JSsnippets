adf4186649af14b1fe75a3fdb112bee8ce45ba00
Gangareddy Nachu <gnachu@horizon.csueastbay.edu>
2023-07-31 21:50:29 -0700

    Update Prototype.js

--- Prototype.js
+++ Prototype.js
@@ -1,32 +1,62 @@
 var TimeLimitedCache = function() {
-  // Define keymap as a property of the instantiated object (this)
-  this.keymap = new Map();
+
+     this.keymap = new Map();
+
+    
 };
 
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
 TimeLimitedCache.prototype.set = function(key, value, duration) {
-  // Access keymap using "this.keymap" since it's a property of the instance
-  this.keymap.set(key, value);
 
-  setTimeout(() => {
-    this.keymap.clear();
-  }, duration);
+    let found = this.keymap.has(key);
+    if(found){
+        clearTimeout(this.keymap.get(key).ref);
+    }
+   
+    this.keymap.set(key,{
+        value,
+        ref: setTimeout(()=> this.keymap.delete(key),duration)
+    });;
+
+    return found;
+
+    
+    
 };
 
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
 TimeLimitedCache.prototype.get = function(key) {
-  if (this.keymap.has(key)) {
-    return this.keymap.get(key);
-  }
-  return -1;
+
+    if(this.keymap.has(key)){
+          return this.keymap.get(key).value;
+    }
+
+    return -1;
+    
 };
 
+/** 
+ * @return {number} count of non-expired keys
+ */
 TimeLimitedCache.prototype.count = function() {
-  return this.keymap.size;
+   return  this.keymap.size;
+    
 };
 
-// Usage:
-var obj = new TimeLimitedCache();
-obj.set(1, 42, 1000);
-obj.get(1); // Returns 42
-obj.count(); // Returns 1
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
 //keymap to be accessible to other functions in the prototype, you need to define it in the constructor function's scope
 //but outside any specific function. This way, it becomes a property of the instantiated objects, and all methods in the prototype can access it.
