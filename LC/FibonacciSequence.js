395925cff5e6acabe8df383020ec6aaf54edfe26
Gangareddy Nachu <gnachu@horizon.csueastbay.edu>
2024-06-16 17:36:50 -0700

    Create FibonacciSequence.js

---
+++ LC/FibonacciSequence.js
@@ -0,0 +1,11 @@
+Fibonacci sequence
+
+0,1,1,2,3,5,8......
+
+  // First two letters are 0 and 1
+  // then from the third letter onwards it follows the sequence f(n) = f(n-1)+f(n-2)
+  /*
+ F0 = 0 (applies only to the first integer)
+F1 = 1 (applies only to the second integer)
+Fn = Fn-1 + Fn-2 (applies to all other integers)
+  */
